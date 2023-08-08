4c7c6d49bf34be2ef53ba4fea1735ca0beb6bd59
mmaxgamma <89859919+maxwell-morris28@users.noreply.github.com>
2023-08-08 01:26:22 -0400

    Updated about me section and added alt text to portrait

--- src/About.jsx
+++ src/About.jsx
@@ -15,7 +15,8 @@ function About() {
                 I was introduced to technology at a very young age, which has fostered an interest that played a huge role in 
                 deciding my career path. As a result, I have gained experience and skills across multiple disciplines, such as 
                 UX Design and web development.</p>
-              <img id='headshot' src="src/Max_Headshot.png"/>
+              <img id='headshot' src="src/Max_Headshot.png" alt='Professional headshot of me smiling while wearing a dark blue blazer 
+              with a light blue shirt'/>
             </div>
             <div className='myMission'>
               <h2 className='aboutSubheader' id='missionHeader'>My Mission</h2>
@@ -26,12 +27,20 @@ function About() {
               <br></br><br></br>
               With technology becoming a more widespread, integral component of our lives, we are given new solutions, but also face more
               problems. I chose to become a UX Designer because I want to leverage my skills in technology to help address the existing
-              and emerging problems of the world, whether it is related to accessibility, the COVID-19 pandemic, or anything else.
+              and emerging problems of the world, whether it is related to accessibility, the COVID-19 pandemic, or anything else. 
               <br></br><br></br>
-              I am quite versatile, also having a skillset in front end web development (ie. HTML, CSS, JavaScript, React and Three.js) 
-              which allows me to bridge the gap between designer and developer. As a matter of fact, the website you are reading 
-              this excerpt from was built from scratch using React! I taught myself this skill to help promote a more seamless workflow 
-              that will allow helping those in need become that much easier.
+              As a UX Designer, I am always learning and willing to learn. But I have also gotten the chance to gain experience in areas 
+              such as user-centered design, UX research methods, UI and Interaction Design, prototyping through Figma, designing for
+              diversity, accessibility and WCAG, as well as the Agile methodology.
+              <br></br><br></br>
+              I am quite detail-oriented, committed to seeing my jobs through to the end without cutting corners. To that end, 
+              I&apos;ve chosen to wear multiple hats through an additional skillset in front end web development (ie. HTML, CSS, JavaScript, 
+              React and Three.js) which allows me to bridge the gap between designer and developer. As a matter of fact, the website 
+              you are reading this excerpt from was built from scratch using React! I taught myself this skill because I am a 
+              team player--I want to help promote a more seamless workflow that will allow helping those in need become that much easier.
+              <br></br><br></br>
+              I hope that by looking through my projects you can gain further insight on who I am as a designer and the passion I have 
+              for what I do.
               </p>
             </div>
             <div className='funFacts'>
